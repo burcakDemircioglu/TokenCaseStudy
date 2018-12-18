8f5ec9631980ce7f79a15c1a4b3933935a8970b3
Burcak Demircioglu <burcakdemircioglu@gmail.com>
2018-12-18 14:57:20 +0300

    Do Basic API Request Functionality

--- app/src/main/java/com/example/burcakdemircioglu/tokencasestudy/MainActivity.java
+++ app/src/main/java/com/example/burcakdemircioglu/tokencasestudy/MainActivity.java
@@ -12,21 +12,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mTextViewResult;
+    private String qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTextViewResult = findViewById(R.id.text_view_result);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mTextViewResult.setText("reset");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -40,6 +60,94 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    public void requestQR(View v) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"totalReceiptAmount\":100}");
+        Request request = new Request.Builder()
+                .url(BuildConfig.CAMPAIGN_QR_API_URL + "get_qr_sale")
+                .post(body)
+                .addHeader("x-ibm-client-id", BuildConfig.X_IBM_CLIENT_ID)
+                .addHeader("x-ibm-client-secret", BuildConfig.X_IBM_CLIENT_SECRET)
+                .addHeader("content-type", "application/json")
+                .addHeader("accept", "application/json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    try {
+                        final String myResponse = response.body().string();
+
+                        JSONObject jsonCell = new JSONObject(myResponse);
+
+                        qr = jsonCell.getString("QRdata");
+
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                mTextViewResult.setText(myResponse);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void requestPayment(View v) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"returnCode\":1000,\"returnDesc\":\"success\",\"receiptMsgCustomer\":\"beko Campaign\",\"receiptMsgMerchant\":\"beko Campaign Merchant\",\"paymentInfoList\":[{\"paymentProcessorID\":67,\"paymentActionList\":[{\"paymentType\":3,\"amount\":100,\"currencyID\":949,\"vatRate\":800}]}],\"QRdata\":\"" + qr + "\"}");
+        Request request = new Request.Builder()
+                .url(BuildConfig.CAMPAIGN_QR_API_URL + "payment")
+                .post(body)
+                .addHeader("x-ibm-client-id", BuildConfig.X_IBM_CLIENT_ID)
+                .addHeader("x-ibm-client-secret", BuildConfig.X_IBM_CLIENT_SECRET)
+                .addHeader("content-type", "application/json")
+                .addHeader("accept", "application/json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+                if (response.isSuccessful()) {
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                } else {
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
